b79d8858020aca3bed87fab17d2be5c9ddac2563
Fidelis Perkonigg <fp910@imperial.ac.uk>
2017-11-07 16:25:54 +0000

    Skeleton files

---
+++ lexicon.pl
@@ -0,0 +1,14 @@
+article([the]).
+
+noun([grass]).
+noun([cow]).
+noun([boy]).
+noun([girl]).
+noun([song]).
+
+verb([eats]).
+verb([chews]).
+verb([kicks]).
+verb([sings]).
+
+animate([cow, boy, girl]).
---
+++ nat.pl
@@ -0,0 +1,29 @@
+%%%%% Natural Language Program
+
+sentence(S) :-
+	noun_phrase(NP),
+	verb_phrase(VP),
+	append(NP, VP, S).
+
+noun_phrase(NP) :-
+	article(A),
+	noun(N),
+	append(A, N, NP).
+
+verb_phrase(V) :-
+	verb(V).
+verb_phrase(VP) :-
+	verb(V),
+	noun_phrase(NP),
+	append(V, NP, VP).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+/* The required predicates and argument positions are:
+
+	a.  conj(Text)
+	b.  encode(Text, EncodedText)
+	c.  same_actor(Text)
+
+*/
